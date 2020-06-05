948174c435924ad6614a0ff32d9887bc28a348da
gandh99 <gandh99@hotmail.com>
2020-06-05 11:46:06 +0800

    Added some ui for the questions page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuestionPageComponent } from './components/question-page/question-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
