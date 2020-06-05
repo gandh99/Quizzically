2730acffcda78cfc358de7e9f3cd49e815e562cd
gandh99 <gandh99@hotmail.com>
2020-06-05 17:25:29 +0800

    Moved all quiz components to the quiz module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,32 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button'
-import { MatRadioModule } from '@angular/material/radio';
-import { IconsModule } from './icons/icons.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { QuestionPageComponent } from './components/question-page/question-page.component';
-import { RadioOptionComponent } from './components/radio-option/radio-option.component';
-import { QuestionCounterComponent } from './components/question-counter/question-counter.component';
-import { QuizComponent } from './quiz/quiz.component';
+import { QuizModule } from './quiz/quiz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionPageComponent,
-    RadioOptionComponent,
-    QuestionCounterComponent,
-    QuizComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatRadioModule,
-    IconsModule
+    QuizModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/quiz/quiz.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { IconsModule } from '../icons/icons.module';
+import { QuestionPageComponent } from './components/question-page/question-page.component';
+import { RadioOptionComponent } from './components/radio-option/radio-option.component';
+import { QuestionCounterComponent } from './components/question-counter/question-counter.component';
+import { QuizComponent } from './quiz.component';
+
+
+@NgModule({
+  declarations: [
+    QuizComponent,
+    QuestionPageComponent,
+    RadioOptionComponent,
+    QuestionCounterComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatRadioModule,
+    IconsModule
+  ],
+  exports: [
+    QuizComponent
+  ]
+})
+export class QuizModule { }
