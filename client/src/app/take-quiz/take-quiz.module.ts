c32e02616d0218dc7fb5526d0d49e8261ebdb4a9
gandh99 <gandh99@hotmail.com>
2020-06-11 09:27:22 +0800

    Created result page

--- client/src/app/take-quiz/take-quiz.module.ts
+++ client/src/app/take-quiz/take-quiz.module.ts
@@ -6,11 +6,12 @@ import { ReusableUiModule } from '../reusable-ui/reusable-ui.module';
 import { QuizStartComponent } from './components/quiz-start/quiz-start.component';
 import { QuizBodyComponent } from './components/quiz-body/quiz-body.component';
 import { QuizOptionComponent } from './components/quiz-option/quiz-option.component';
+import { QuizResultComponent } from './components/quiz-result/quiz-result.component';
 
 
 
 @NgModule({
-  declarations: [TakeQuizMainComponent, QuizStartComponent, QuizBodyComponent, QuizOptionComponent],
+  declarations: [TakeQuizMainComponent, QuizStartComponent, QuizBodyComponent, QuizOptionComponent, QuizResultComponent],
   imports: [
     CommonModule,
     IconsModule,
