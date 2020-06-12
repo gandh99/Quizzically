f200d5313c5198f9b2eb6d3242c0ccc8a4b89657
gandh99 <gandh99@hotmail.com>
2020-06-12 15:39:50 +0800

    Added skeleton files for server

---
+++ server/src/main/java/com/gandh99/quizically/QuizicallyApplication.java
@@ -0,0 +1,13 @@
+package com.gandh99.quizically;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizicallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizicallyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/gandh99/quizically/QuizicallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gandh99.quizically;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizicallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
